e2720f26bc16e260ad00cbaf8dcde7da4089a69a
igorsimjanoski <ige.simjanoski@gmail.com>
2016-05-29 15:57:33 +0200

    rename of class and method name

--- bin/test_goeuro.py
+++ bin/test_goeuro.py
@@ -4,22 +4,12 @@ import time
 import sys
 import unittest
 
-class Selenium2OnLocal(unittest.TestCase):
-    '''
-    Test is using firefox driver. FLow is like this:
-    -open browser
-    -insert text into to/from fields
-    -click on search
-    -wait until results are ready(this can be tricky, in this case test check for class attribute on one of elements to change to specific value)
-    -extract price from results
-    -check if results are sorted
-    -close browser
-    
-    '''
+class TrainResults(unittest.TestCase):
+
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def test_from_local(self):
+    def test_search(self):
         print "opening browser"
         self.driver.get('http://www.goeuro.com/')
         print "browse opened"
